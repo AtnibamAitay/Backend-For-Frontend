9414650f8d5e105b6d2fb5a476d1fb88f2489716
AtnibamAitay <atnibamaitay@foxmail.com>
2024-02-15 16:32:59 +0800

    feat(api-ums): 新增api-ums模块
    - 添加了api-ums模块，实现了用户信息相关的API接口功能
    
    本次提交新增了api-ums模块，以支持远程调用用户模块相关的接口。

---
+++ bcp-api/api-ums/src/main/java/space/atnibam/api/ums/RemoteUserInfoService.java
@@ -0,0 +1,34 @@
+package space.atnibam.api.ums;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import space.atnibam.common.core.domain.R;
+
+import java.util.List;
+
+/**
+ * 用户信息远程调用接口
+ */
+@FeignClient(value = "modules-ums", contextId = "userInfo", url = "http://local.atnibam.space:9030")
+public interface RemoteUserInfoService {
+    /**
+     * 根据用户id查出用户名、用户头像、用户简介
+     *
+     * @param userId 用户ID
+     * @return 用户信息DTO
+     */
+    @GetMapping("/api/userInfo/{userId}")
+    R getDetailedUserInfo(@PathVariable(value = "userId") Integer userId);
+
+    /**
+     * 根据一个或多个用户id查出用户名、用户头像列表
+     *
+     * @param userIds 用户ID列表
+     * @return 用户信息DTO列表
+     */
+    @PostMapping("/api/userInfo/basic-info")
+    R getBasicUserInfo(@RequestBody List<Integer> userIds);
+}
