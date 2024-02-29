019d956cc6a92b1ee54159fae70edf10323ba007
AtnibamAitay <atnibamaitay@foxmail.com>
2024-02-29 22:24:45 +0800

    feat(bff-steamedu): 引入common-security模块并配置全局异常处理
    - 在`bff-modules/bff-steamedu/pom.xml`中添加对`space.atnibam.common.security`依赖
    - 导入`SecurityRedisConfig`和`GlobalExceptionHandler`类到`SteameduApplication.java`以实现安全相关的Redis配置及全局异常处理机制

--- bff-modules/bff-steamedu/src/main/java/space/atnibam/steamedu/SteameduApplication.java
+++ bff-modules/bff-steamedu/src/main/java/space/atnibam/steamedu/SteameduApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import space.atnibam.common.security.config.SecurityRedisConfig;
+import space.atnibam.common.security.handler.GlobalExceptionHandler;
 import space.atnibam.common.swagger.annotation.EnableCustomSwagger;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
@@ -14,6 +17,7 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 @EnableFeignClients(basePackages = "space.atnibam.api")
 @EnableOpenApi
 @MapperScan("space.atnibam.**.mapper")
+@Import({SecurityRedisConfig.class, GlobalExceptionHandler.class})
 public class SteameduApplication {
     public static void main(String[] args) {
         SpringApplication.run(SteameduApplication.class, args);
